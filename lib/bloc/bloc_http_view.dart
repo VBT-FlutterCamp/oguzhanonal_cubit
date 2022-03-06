618d200fe2225b3a286b1b6b30f28b7fe6eb38e9
Oğuzhan ÖNAL <oguzhanonal52@hotmail.com>
2022-03-06 22:43:43 +0300

    Baslangic

---
+++ lib/bloc/bloc_http_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bloc extends StatefulWidget {
+  Bloc({Key? key}) : super(key: key);
+
+  @override
+  State<Bloc> createState() => _BlocState();
+}
+
+class _BlocState extends State<Bloc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
